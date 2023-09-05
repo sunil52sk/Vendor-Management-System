0db4c4ec85bc2c1a55165153c107fd83dd99dd5a
sunilkumar_seenivasan <100457242+sunil52sk@users.noreply.github.com>
2023-09-05 10:46:57 +0530

    Delete VMS directory

--- VMS/src/main/java/com/example/springapp/repository/ProjectRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.springapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.springapp.model.ProjectsModel;
-
-public interface ProjectRepo extends JpaRepository<ProjectsModel, Integer>{
-	
-}
